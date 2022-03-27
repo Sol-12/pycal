4599da77266fa7096acd7e37bb4d470ed5bbf2ab
Cr4wley <brencic.rok@gmail.com>
2022-03-27 18:18:01 +0200

    Display events

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
-from print_calendar import print_calendar
 from datetime import datetime
+
+from print_calendar import print_calendar
+from print_calendar import get_month_string
 
 # Loaded JSON data 
 data = None
@@ -10,8 +12,20 @@ def load_data():
     global data
 
     file_pointer = open("calendar.json")
-    data = json.load(file_pointer)
+    raw_data = json.load(file_pointer)
     file_pointer.close()
+
+    data = []
+    for event in raw_data:
+        raw_event_datetime = event["datetime"]
+        event_datetime = datetime.fromisoformat(raw_event_datetime)
+        parsed_event = {
+                "id": event["id"],
+                "datetime": event_datetime,
+                "title": event["title"],
+                "content": event["content"]
+        }
+        data.append(parsed_event)
 
 # Creates 'example_calendar.json' file with an example of an json formatted calendar
 def make_example_calendar_json():
@@ -30,18 +44,62 @@ def make_example_calendar_json():
 def get_dates_with_events():
     output = []
     for event in data:
-        raw_event_datetime = event["datetime"]
-        event_datetime = datetime.fromisoformat(raw_event_datetime)
-
-        output.append(event_datetime)
+        output.append(event["datetime"])
 
     return output
+
+def is_same_date(day, month, year, date):
+    return date.day == day and date.month == month and date.year == year
+
+def get_events_for_date(day, month, year):
+    output = []
+
+    for event in data:
+        if is_same_date(day, month, year, event["datetime"]):
+            output.append(event)
+
+    return output
+
+def format_time(date):
+    hour = date.hour
+    minute = date.minute
+
+    if hour < 10:
+        hour = "0" + str(hour)
+    else:
+        hour = str(hour)
+
+    if minute < 10:
+        minute = "0" + str(minute)
+    else:
+        minute = str(minute)
+
+    output = hour + ":" + minute
+    return output
+
+def print_selected_day_events(selected_year, selected_month, selected_day):
+    selected_day_events = get_events_for_date(selected_day, selected_month, selected_year)
+    print()
+    print("# " + get_month_string(selected_month - 1) + " " + str(selected_day))
+    print("## EVENTS:")
+
+    for event in selected_day_events:
+        print("- ", end='')
+        print("(", end='')
+        print(format_time(event["datetime"]), end='')
+        print(")", end='')
+        print(" [" ,end='')
+        print(event["id"], end='')
+        print("] " ,end='')
+        print(event["title"])
+        print(event["content"])
+
 
 def main():
     # Load arguments
     selected_year = 2022
     selected_month = 3
-    selected_day = 26
+    selected_day = 27
 
     # Load data
     load_data()
@@ -51,6 +109,9 @@ def main():
 
     # Display calendar
     print_calendar(selected_year, selected_month, selected_day, dates_with_events)
+
+    # Display events if any
+    print_selected_day_events(selected_year, selected_month, selected_day)
 
 if __name__ == '__main__':
     main()
