e6d9abdf7298ddb90847915ae2edbe5660782aac
Cr4wley <brencic.rok@gmail.com>
2022-03-29 22:56:19 +0200

    added aliases

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias sc='/home/cr4wley/Programs/pycal/show_cal'
+alias rmcal='/home/cr4wley/Programs/pycal/remove_cal_event.py'
+alias addcal='/home/cr4wley/Programs/pycal/add_cal_event.py'
+alias pc='/home/cr4wley/Programs/pycal/pycal.py'
